8e2ab8b2625257f4bd24bcf9a3881204586699ed
Travis Webb <me@traviswebb.com>
2015-06-18 19:47:11 -0400

    added stuff

---
+++ bookmark.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+  var g = document.createElement('script');
+  g.setAttribute('src','https://raw.githubusercontent.com/tjwebb/guyify/master/fun.js');
+  document.body.appendChild(g);
+}());
